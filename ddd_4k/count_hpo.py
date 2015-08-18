69e48098101ba400c9e0e8fd427f39d0ba9c5db8
Jeremy McRae <jm33@sanger.ac.uk>
2015-08-18 13:47:35 +0100

    added function to count HPO terms per person

---
+++ ddd_4k/count_hpo.py
@@ -0,0 +1,39 @@
+"""
+Copyright (c) 2015 Wellcome Trust Sanger Institute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+def count_hpo_terms(pheno, person):
+    """ count the number of HPO terms per person
+    
+    Args:
+        pheno: DataFrame, containing columns like "child_hpo", "mother_hpo"
+        person: which trio member to extract counts for e.g. "child"
+    
+    Returns:
+        pandas Series of counts of HPO terms per person
+    """
+    
+    def f(x):
+        if x == "NA":
+            return 0
+        else:
+            return len(x.split("|"))
+    
+    return pheno["{}_hpo".format(person)].apply(f)
