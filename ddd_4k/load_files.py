cd3074551ed52175e61aae3cdcdd9fe6c5ccbbda
Jeremy McRae <jm33@sanger.ac.uk>
2015-08-18 11:46:34 +0100

    added functions to load phenotype file

--- ddd_4k/load_files.py
+++ ddd_4k/load_files.py
@@ -52,3 +52,44 @@ def open_de_novos(path):
     de_novos.sex[de_novos.sex == "F"] = "female"
     
     return de_novos
+
+def open_known_genes(path):
+    """ open the dataset of known developmental disorder genes
+    
+    Args:
+        path: path to known developmental disorder genes data file.
+    
+    Returns:
+        DataFrame for the known genes.
+    """
+    
+    genes = pandas.read_table(path, sep="|", na_filter=False)
+    genes = genes[genes["ddg2p_status"] != "Possible DD Gene"]
+    
+    return genes
+
+def open_phenotypes(pheno_path, alt_ids_path=None):
+    """ load the phenotypes dataset, and merge the alternate IDs
+    
+    Args:
+        pheno_path: path to phenotypes data file.
+        alt_ids_path: path to data file containing alternate IDs.
+    
+    Returns:
+        DataFrame for the phenotypes.
+    """
+    
+    pheno = pandas.read_table(pheno_path, na_filter=False)
+    
+    if alt_ids_path is not None:
+        # load the alternate IDs for the individuals. Remove the parental IDs, so we
+        # can convert the decipher ID to an integer, so we can merge the dataframe
+        # with the phenotypes dataframe, which stores the decipher ID column as an
+        # integer
+        alt_ids = pandas.read_table(alt_ids_path, na_filter=False)
+        alt_ids = alt_ids[~alt_ids["decipher_id"].str.contains(":")]
+        alt_ids["decipher_id"] = alt_ids["decipher_id"].astype(int)
+        
+        pheno = pheno.merge(alt_ids, how="left", left_on="patient_id", right_on="decipher_id")
+    
+    return pheno
