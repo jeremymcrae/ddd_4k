bd81a498bcc9ef5a19d145541cf62b67183d5172
Jeremy McRae <jm33@sanger.ac.uk>
2015-08-19 14:09:35 +0100

    added function to convert number of months/weeks/years to seconds

---
+++ ddd_4k/convert_durations.py
@@ -0,0 +1,68 @@
+"""
+Copyright (c) 2015 Wellcome Trust Sanger Institute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
+from datetime import timedelta
+
+import pandas
+
+def get_duration(value):
+    """ standardises a duration string into number of seconds
+    
+    Many children have the time from birth until they achieved several
+    developmental milestones, such as smiled, sat up, walked and spoke their
+    first words. These can be recorded as number of weeks, months, years, and
+    can mix the units within each achievement. We need to standardise these to
+    be able to analyse differences in groups for these achievements.
+    
+    Args:
+        value: the string value for the duration. These can indicate a number of
+        weeks, months, years etc. For example:
+            weeks: "1 week", 6 weeks, "52 weeks or more"
+            months: "7 months", "23 months"
+            years: "2-2.5 years", "3-4 years", "5 years and over"
+            missing values: "Unknown", "NA", "Not yet achieved"
+    
+    Returns:
+        the total number of seconds in the duration
+    """
+    
+    weeks_per_month = 4.33
+    weeks_per_year = 52.18
+    
+    if pandas.isnull(value):
+        return None
+    elif not any([ x in value for x in ["week", "month", "year"] ]):
+        return None
+    
+    # get the numeric value from the string
+    count = value.split(" ")[0]
+    if "-" in value:
+        count = count.split("-")[1]
+    count = float(count)
+    
+    if "week" in value:
+        duration = timedelta(weeks=count)
+    elif "month" in value:
+        duration = timedelta(weeks=count * weeks_per_month)
+    elif "year" in value:
+        duration = timedelta(weeks=count * weeks_per_year)
+    
+    return duration.total_seconds()
