8335eb73e0c88e65170292c2432092a589cee598
Jeremy McRae <jm33@sanger.ac.uk>
2015-09-15 10:29:18 +0100

    convert categorical plots to plotting ratios
     - known genes now only include genes with monoallelic modes of
       inheritance.
     - categorical plots now plot ratios of group tallies to the total
       number of individuals found in the group (that could contribute
       to the tally).
     - categorical plots now include a table of results from Fisher's
       exact test.

--- scripts/mutations_by_phenotype.py
+++ scripts/mutations_by_phenotype.py
@@ -24,6 +24,9 @@ import numpy
 import matplotlib
 matplotlib.use('Agg')
 import seaborn
+import pandas
+from scipy.stats import fisher_exact
+from matplotlib import pyplot
 
 from ddd_4k.load_files import open_de_novos, open_known_genes, open_phenotypes, \
     open_families
@@ -38,15 +41,67 @@ from ddd_4k.scale_durations import autoscale_durations
 seaborn.set_context("notebook", font_scale=2)
 seaborn.set_style("white", {"ytick.major.size": 10, "xtick.major.size": 10})
 
+def get_categorical_summary(merged, cohort_counts, value):
+    """ count the number of individuals by known gene status and functional
+    de novo status. Include the totoal number of individuals in each group, so
+    we can estimate ratios, and perform Fisher's exact test to compare groups.
+    
+    Args:
+        merged: pandas DataFrame listing the indivioduals with de novos by
+            functional status and known  gene status, along with their
+            phenotypic data, so we can group by the required "value".
+        cohort_counts: pandas Series listing the total number of individuals in
+            the cohort for the different groups split by the "value".
+        value: the phenotypic value that we are grouping by.
+    
+    Returns:
+        pandas DataFrame containing tallies of individuals by functional de novo
+        status and known gene status, for the different "value" groups.
+    """
+    
+    summary = pandas.DataFrame({"known": [], value: [], "consequence": [], \
+        "tally": [], "total": []})
+    
+    for known in [True, False]:
+        for x in cohort_counts.keys():
+            temp = merged[(merged.known == known) & (merged[value] == x)]
+            tally = temp["consequence"].value_counts()
+            total = [cohort_counts[x]] * len(tally)
+            cq = list(tally.keys())
+            
+            temp = pandas.DataFrame({"known": [known] * len(tally), \
+                value: [x] * len(tally), "consequence": cq, "tally": tally, \
+                "total": total})
+            summary = summary.append(temp)
+    
+    summary = summary.reset_index(drop=True)
+    summary["ratio"] = summary["tally"]/summary["total"]
+    
+    return summary
+
 def plot_categorical(counts, pheno, value):
     """ plot probands per category by consequence by known gene status
     """
     
     merged = counts.merge(pheno[["person_stable_id", value]], on="person_stable_id")
     
-    fig = seaborn.factorplot(x="consequence", hue=value, col="known", data=merged, size=6, kind="count")
+    cohort_counts = pheno[value].value_counts()
+    summary = get_categorical_summary(merged, cohort_counts, value)
+    
+    # test whether differences exist between the groups for the value column
+    results = []
+    for known in [True, False]:
+        for cq in summary.consequence.unique():
+            table = summary[(summary.known == known) & (summary.consequence == cq)]
+            table = table[["tally", "total"]]
+            odds_ratio, p_value = fisher_exact(table)
+            results.append([known, cq, p_value])
+    
+    fig = seaborn.factorplot(x="consequence", y="ratio", hue=value, col="known", data=summary, kind="bar")
     fig.set_ylabels("Frequency")
+    pyplot.table(cellText=results, colLabels=["known", "cq", "P"], loc="top right")
     fig.savefig("results/{}_by_consequence.pdf".format(value), format="pdf")
+    
     matplotlib.pyplot.close()
 
 def plot_quantitative(counts, pheno, value, y_label):
@@ -93,7 +148,8 @@ def plot_achievement(counts, pheno, achievement):
 def main():
     de_novos = open_de_novos(DENOVO_PATH)
     known = open_known_genes(KNOWN_GENES)
-    de_novos["known"] = de_novos["symbol"].isin(known["gencode_gene_name"])
+    monoallelic = known[known["mode"].isin(["Monoallelic", "X-linked dominant"])]
+    de_novos["known"] = de_novos["symbol"].isin(monoallelic["gencode_gene_name"])
     
     # For each proband, count the number of functional de novos (split by lof
     # and missense), in known developmental disorder genes (and other genes).
