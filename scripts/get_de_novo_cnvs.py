d6086998fffa03380d87db03a6e45537f58d9dd9
Jeremy McRae <jm33@sanger.ac.uk>
2015-10-06 18:45:53 +0100

    assign correct copyright owner

--- scripts/get_de_novo_cnvs.py
+++ scripts/get_de_novo_cnvs.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2015 Wellcome Trust Sanger Institute
+Copyright (c) 2015 Genome Research Ltd.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
@@ -18,6 +18,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+
+from __future__ import print_function, division
 
 import sys
 import gzip
