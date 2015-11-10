1003909831e357de290c75e18c4155840dcf3d23
Jeremy McRae <jm33@sanger.ac.uk>
2015-11-10 13:18:47 +0000

    better prepare variants table, including HGVS codes where available

--- scripts/prepare_gene_reports.py
+++ scripts/prepare_gene_reports.py
@@ -23,6 +23,7 @@ import argparse
 import getpass
 import json
 
+import pandas
 import psycopg2
 from tabulate import tabulate
 import markdown
@@ -90,6 +91,7 @@ class EnsemblVariant(EnsemblRequest):
         """ extract the HGVS predictions for a variant
         """
         
+        self.request_attempts = 0
         end_pos = pos + len(ref) - 1
         
         headers = {"Content-Type": "application/json"}
@@ -97,22 +99,20 @@ class EnsemblVariant(EnsemblRequest):
         ext = "/vep/human/region/{}/{}?hgvs=1;canonical=1".format(coordinate, alt)
         
         r = self.ensembl_request(ext, coordinate, headers)
-        
         variant = json.loads(r)[0]
         
         return self.parse_alternate_consequences(variant, gene, vep_cq)
 
-def get_variant_details(variants, ensembl, cur):
-    """
+def get_clinician_details(sample_ids, cur):
+    """ get details of clinicians who recruited each proband into the DDD
     
     Args:
+        sample_ids: list of DDD sample IDs
         cur: sql cursor, to access the DDD database
+    
+    Returns:
+        dictionary of clinician details, indexed by DDD sample ID.
     """
-    
-    for variant in variants:
-        ensembl.get_variant_hgvs(chrom, pos, ref, alt, hgnc, consequence)
-    
-    sample_ids = list(variants["person_id"])
     
     cur.execute("select p.stable_id,p.decipher_id,clin.* " \
         "from fe.person p join clinician clin using(id_clinician) " \
@@ -120,19 +120,55 @@ def get_variant_details(variants, ensembl, cur):
     
     query = cur.fetchall()
     
+    # reformat the table into a dictionary, so we can pull out the details given
+    # a sample ID
     clinicians = {}
     for x in query:
-        decipher_id = x[1]
-        clinicians[decipher_id] = {}
-        clinicians[decipher_id]["email"] = x[3]
-        clinicians[decipher_id]["clinician"] = x[4]
+        sample_id = x[0]
+        clinicians[sample_id] = {}
+        clinicians[sample_id]["decipher_id"] = x[1]
+        clinicians[sample_id]["email"] = x[3]
+        clinicians[sample_id]["clinician"] = x[4]
     
-    # we need decipher ID, sex, variant location, ref and alt alleles, vep
-    # consequence, gene symbol, protein consequence (e.g. "p.Arg222*")referring
-    # clinician, validation status. HGVS for transcript and protein,
-    #
+    return clinicians
+
+def get_variant_details(variants, ensembl, cur):
+    """
+    we need decipher ID, sex, variant location, ref and alt alleles, vep
+    consequence, gene symbol, protein consequence (e.g. "p.Arg222*"), referring
+    clinician, validation status. HGVS for transcript and protein,
     
-    return md
+    Args:
+        variants: pandas DataFrame of variants, with sample IDs, coordiantes,
+            alleles, consequence and validation statuses.
+        ensembl: EnsemblVariant object, for pulling HGVS nomenclature out.
+        cur: sql cursor, to access the DDD database
+    """
+    
+    sample_ids = list(variants["person_stable_id"])
+    clinicians = get_clinician_details(sample_ids, cur)
+    
+    variants["transcript_hgvs"] = None
+    variants["protein_hgvs"] = None
+    variants["clinician"] = None
+    variants["clinician_email"] = None
+    variants["decipher_id"] = None
+    for idx, var in variants.iterrows():
+        hgvs = ensembl.get_variant_hgvs(var["chrom"], var["pos"], var["ref"],
+            var["alt"], var["symbol"], var["consequence"])
+        
+        if hgvs is not None:
+            if "hgvsp" in hgvs:
+                variants["protein_hgvs"].loc[idx] = hgvs["hgvsp"]
+            if "hgvsc" in hgvs:
+                variants["transcript_hgvs"].loc[idx] = hgvs["hgvsc"]
+        
+        sample_id = var["person_stable_id"]
+        variants["clinician"][idx] = clinicians[sample_id]["clinician"]
+        variants["clinician_email"][idx] = clinicians[sample_id]["email"]
+        variants["decipher_id"][idx] = clinicians[sample_id]["decipher_id"]
+    
+    return variants
 
 def get_anthropometric(sample_ids, pheno):
     """ prepare a markdown formatted table of anthropometric data for the probands
@@ -195,10 +231,6 @@ def main():
             var_table = get_variant_details(variants, ensembl, cur)
     
     phenotypes = open_phenotypes(PHENOTYPES, SANGER_IDS)
-    
-    # sample_ids = ["DDDP111164", "DDDP102163"]
-    # decipher_ids = phenotypes["decipher_id"][phenotypes["person_stable_id"].isin(sample_ids)]
-    # decipher_ids = [ str(x) for x in decipher_ids ]
     
     md_text = ""
     md_text += get_anthropometric(variants["person_id"], pheno)
