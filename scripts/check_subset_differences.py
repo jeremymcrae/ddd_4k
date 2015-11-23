5c6205acfc9e7050a87284e7eebeaae8618ee6d8
Jeremy McRae <jm33@sanger.ac.uk>
2015-11-23 11:02:37 +0000

    shifted script to compare multiple modified subsets to the baseline

--- scripts/check_subset_differences.py
+++ scripts/check_subset_differences.py
@@ -20,8 +20,10 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 import argparse
+import os
 
 from numpy import log10, median, mean, logical_not
+from statsmodels.stats.multitest import fdrcorrection
 import pandas
 
 import matplotlib
@@ -44,8 +46,8 @@ def get_options():
         "in developmental disorders.")
     parser.add_argument("--baseline", \
         help="Path to file containing enrichment results for the baseline.")
-    parser.add_argument("--modified", \
-        help="Path to file containing enrichment results for the modified subset.")
+    parser.add_argument("--modified", nargs="+", \
+        help="Paths to files containing enrichment results for the modified subsets.")
     parser.add_argument("--known-genes", default=KNOWN_GENES,
         help="Path to table of genes known to be involved in developmental disorders.")
     parser.add_argument("--output", default="temp.pdf",
@@ -59,16 +61,19 @@ def load_results(path, known_genes):
     """
     """
     
-    dominant = known_genes["gencode_gene_name"][known_genes["mode"].isin(["Monoallelic", "X-linked dominant"])].unique()
+    dominant_modes = ["Monoallelic", "X-linked dominant"]
+    symbols = known_genes["gencode_gene_name"]
+    dominant = symbols[known_genes["mode"].isin(dominant_modes)].unique()
     
     table = pandas.read_table(path, sep="\t")
     
     table["dominant"] = table["hgnc"].isin(dominant)
-    table["log_p"] = -log10(table["enrichment_p_value"])
+    table["log_p"] = -log10(table[["p_lof", "p_func"]].min(axis=1))
     table = table.dropna()
     
+    table = table[["hgnc", "dominant", "log_p"]]
+    
     return table
-
 
 def main():
     
@@ -77,25 +82,47 @@ def main():
     known_genes = open_known_genes(args.known_genes)
     
     baseline = load_results(args.baseline, known_genes)
-    modified = load_results(args.modified, known_genes)
+    modified = [ load_results(x, known_genes) for x in args.modified ]
     
     baseline["baseline"] = baseline["log_p"]
-    modified["modified"] = modified["log_p"]
+    baseline = baseline.drop("log_p", axis=1)
     
-    merged = baseline[["hgnc", "dominant", "baseline"]].merge(modified[["hgnc", "dominant", "modified"]], how="outer", \
-        on=["hgnc", "dominant"])
+    # rename the significance column for the subsets with part of the filename
+    modified_names = [ os.path.basename(x).split(".")[3] for x in args.modified ]
+    for (x, table) in enumerate(modified):
+        name = "modified.{}".format(modified_names[x])
+        table[name] = table["log_p"]
     
-    merged = merged[merged["baseline"] > -log10(THRESHOLD)]
+    modified = [ x.drop("log_p", axis=1) for x in modified ]
+    
+    # merge all the dataframes from the various modified subsets together
+    current = modified[0][["hgnc", "dominant"]]
+    for frame in modified:
+        current = current.merge(frame, how="outer", on=["hgnc", "dominant"])
+    
+    merged = baseline.merge(current, how="outer", on=["hgnc", "dominant"])
     merged = merged.dropna()
     
-    merged["delta"] = merged["modified"] - merged["baseline"]
-    print(median(merged["delta"][merged["dominant"]]))
-    print(median(merged["delta"][merged["dominant"] == False]))
+    # Determine the set of genes for comparison. For this, I've decided on the
+    # genes that have FDR < 0.05, but which are not genomewide, since these are
+    # the genes which might be expected to shift in significance when additional
+    # cohorts are included.
+    fdr = fdrcorrection(10**-merged["baseline"])[1]
+    nearly = merged[(fdr < 0.05) & (merged["baseline"] < -log10(THRESHOLD))]
     
-    fig = seaborn.factorplot(x="dominant", y="delta", data=merged, kind="violin")
+    # figure out the difference in p-values between the modified subsets versus
+    # the baseline p-value.
+    columns = [ x for x in nearly.columns if "modified" in x ]
+    for x in columns:
+        name = x.replace("modified.", "")
+        nearly[name] = merged[x] - merged["baseline"]
+    
+    melted = pandas.melt(nearly, id_vars=["hgnc", "dominant"], \
+        value_vars=modified_names, value_name="delta")
+    
+    fig = seaborn.factorplot(x="dominant", y="delta", hue="variable",
+        data=melted, kind="violin", size=8, aspect=1.5, legend_out=False, width=1)
     fig.savefig(args.output, format="pdf")
-    
-    
-    
+
 if __name__ == '__main__':
     main()
