dd6037de7b40a5308739bf29f3c89f991e4e0b4e
Jeremy McRae <jm33@sanger.ac.uk>
2016-02-03 11:06:26 +0000

    tidy up recognisability code

--- scripts/plot_neurodevelopmental_recognisability.py
+++ scripts/plot_neurodevelopmental_recognisability.py
@@ -41,6 +41,7 @@ seaborn.set_context("notebook", font_scale=2)
 seaborn.set_style("white", {"ytick.major.size": 10, "xtick.major.size": 10})
 
 RATES_PATH = "/lustre/scratch113/projects/ddd/users/jm33/de_novos.ddd_4k.mutation_rates.2015-11-24.txt"
+NEURODEV_PATH = "/nfs/users/nfs_j/jm33/neurodevelopmental.dominant_lof_DRF.xlsx"
 
 def load_neurodevelopmental(path):
     """ load the dominant LoF neurodevelopmental genes, with recognizability
@@ -109,38 +110,36 @@ def estimate_missing_variants(neurodev):
     return delta_ratio * expected_sum
 
 def main():
-    neurodev_path = "/nfs/users/nfs_j/jm33/neurodevelopmental.dominant_lof_DRF.xlsx"
     
     rates = get_ddd_rates(RATES_PATH)
     
-    # TODO: check sex counts
-    male = 2407
-    female = 1887
-    expected = get_expected_mutations(rates, male, female)
-    expected["expected"] = expected[["lof_indel", "lof_snv", "missense_indel",
-        "missense_snv"]].sum(axis=1)
+    # determine the number of mutations we expect per gene, given consequence
+    # specific mutation rates for each gene.
+    expected = get_expected_mutations(rates, male=2407, female=1887)
+    columns = ["lof_indel", "lof_snv", "missense_indel", "missense_snv"]
+    expected["expected"] = expected[columns].sum(axis=1)
     
-    expected = expected[["hgnc", "expected"]]
+    expected = dict(zip(expected["hgnc"], expected["expected"]))
     
     counts = count_de_novos(DENOVO_PATH, VALIDATIONS)
-    neurodev = load_neurodevelopmental(neurodev_path)
+    neurodev = load_neurodevelopmental(NEURODEV_PATH)
     
     neurodev["observed"] = 0
     for hgnc in counts:
         if hgnc in neurodev["hgnc"].values:
             neurodev["observed"][neurodev["hgnc"] == hgnc] = counts[hgnc]
     
-    # plot LoF counts by mutation rates, shade by recognizability
-    neurodev = neurodev.merge(expected, how="left", on=["hgnc"])
-    
     # calculate the observed/expected ratio for individual genes
+    neurodev["expected"] = neurodev["hgnc"].map(expected)
     neurodev["ratio"] = neurodev["observed"]/neurodev["expected"]
     
+    # some genes lack an expected count, since we don't have mutation rates for
+    # genes which lack any mutations in them. Their ratio must be 0.
     neurodev["ratio"][neurodev["ratio"].isnull()] = 0
     
-    # join the group "1" with group "2", since both groups have fewer genes than
-    # the other groups.
-    recode = {1: "1+2", 2: "1+2", 3:3, 4:4, 5:5}
+    # Join the two least recognisable groups, since they are smaller than the
+    # other groups.
+    recode = {1: "1+2", 2: "1+2", 3: 3, 4: 4, 5: 5}
     neurodev["Recognisable"] = neurodev["Recognisable"].map(recode)
     
     plot_recognisability(neurodev)
