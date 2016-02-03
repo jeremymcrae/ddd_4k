33726df5c8df7ebcb38e01daf960cb23d4da4e38
Jeremy McRae <jm33@sanger.ac.uk>
2016-02-03 10:46:43 +0000

    update script to check clinical recognisability to estimate values form aggregate values from recognisability groups

--- scripts/plot_neurodevelopmental_recognisability.py
+++ scripts/plot_neurodevelopmental_recognisability.py
@@ -24,23 +24,23 @@ import sys
 import tempfile
 
 import pandas
-import scipy
-from numpy import log10
+from numpy import median, mean
 import matplotlib
 matplotlib.use("Agg")
 
 import seaborn
 
-from hpo_similarity.ontology import Ontology
-from hpo_similarity.similarity import CalculateSimilarity
-
 from ddd_4k.constants import VALIDATIONS, DENOVO_PATH
 from ddd_4k.load_files import open_de_novos
-from ddd_4k.convert_doi import open_url
+
+from mupit.mutation_rates import get_default_rates, get_expected_mutations
+from mupit.open_ddd_data import get_ddd_rates
 
 # define the plot style
 seaborn.set_context("notebook", font_scale=2)
 seaborn.set_style("white", {"ytick.major.size": 10, "xtick.major.size": 10})
+
+RATES_PATH = "/lustre/scratch113/projects/ddd/users/jm33/de_novos.ddd_4k.mutation_rates.2015-11-24.txt"
 
 def load_neurodevelopmental(path):
     """ load the dominant LoF neurodevelopmental genes, with recognizability
@@ -50,30 +50,11 @@ def load_neurodevelopmental(path):
     
     return neurodev
 
-def load_rates(url):
-    """ load the mutation rates for these genes
-    """
-    
-    # the version of pandas I have cannot natively load excel files from urls.
-    # So we have to downalod the file, save it, then read from the file on disk.
-    response, status, header = open_url(url, headers={})
-    
-    if status != 200:
-        sys.exit("Failed to load Samocha supplementary rates from url")
-    
-    with tempfile.NamedTemporaryFile(mode="wb") as handle:
-        handle.write(response)
-        handle.flush()
-        rates = pandas.read_excel(handle.name, sheetname="mutation_probabilities")
-    
-    return rates
-
 def count_de_novos(de_novo_path, validations_path):
     """ count the loss-of-function mutations for each gene.
     """
     
     de_novos = open_de_novos(de_novo_path, validations_path)
-    # de_novos = de_novos[de_novos["category"] == "loss-of-function"]
     
     counts = {}
     for (gene, rows) in de_novos.groupby("hgnc"):
@@ -81,30 +62,91 @@ def count_de_novos(de_novo_path, validations_path):
     
     return counts
 
+def plot_recognisability(neurodev):
+    """ plot the observed expected ratios at different recognisability indexes
+    """
+    
+    fig = seaborn.factorplot(x="Recognisable", y="ratio", data=neurodev,
+        kind="box", order=["1+2", 3, 4, 5], size=6, fliersize=0)
+    
+    lim = fig.ax.set_ylim((0, 80))
+    
+    lab = fig.ax.set_ylabel("observed/expected")
+    lab = fig.ax.set_xlabel("Clinical recognisability")
+    
+    fig.savefig("clinical_recognisability.pdf", format="pdf")
+    matplotlib.pyplot.close()
+
+def estimate_missing_variants(neurodev):
+    """ estimate the number of missing neurodevelopmental genes
+    
+    Args:
+        neurodev: pandas dataframe of neurodevelopmental genes, with their
+            clinical recognisability.
+        baseline: baseline proportion of observed to expected mutations at
+            lowest clinical recognisability.
+        slope: slope of linear regression, so that each increment of
+            recognisability reduces the baseline proportion by this much.
+        
+    Returns:
+        number of missing genes
+    """
+    
+    neurodev = neurodev[~neurodev["ratio"].isnull()].copy()
+    
+    baseline = median(neurodev["ratio"][neurodev["Recognisable"] != 5])
+    recognisable = median(neurodev["ratio"][neurodev["Recognisable"] == 5])
+    
+    delta_ratio = (baseline - recognisable)
+    
+    # figure out how many we should have expected in the group of most clinically
+    # recognisable genes.
+    expected_sum = neurodev["expected"][neurodev["Recognisable"] == 5].sum()
+    
+    # count how many were missing from those genes, given the difference in
+    # obs/exp ratio for the most recognisable genes to the less recognisable
+    # genes
+    return delta_ratio * expected_sum
+
 def main():
-    rates_url = "http://www.nature.com/ng/journal/v46/n9/extref/ng.3050-S2.xls"
     neurodev_path = "/nfs/users/nfs_j/jm33/neurodevelopmental.dominant_lof_DRF.xlsx"
     
-    rates = load_rates(rates_url)
-    rates["splice_site"][rates["splice_site"].isnull()] = -300
-    rates["rate"] = log10(10**rates["mis"] + 10**rates["non"] +
-        10**rates["splice_site"] + 10**rates["frameshift"])
-    rates = rates[["gene", "rate"]]
+    rates = get_ddd_rates(RATES_PATH)
+    
+    # TODO: check sex counts
+    male = 2407
+    female = 1887
+    expected = get_expected_mutations(rates, male, female)
+    expected["expected"] = expected[["lof_indel", "lof_snv", "missense_indel",
+        "missense_snv"]].sum(axis=1)
+    
+    expected = expected[["hgnc", "expected"]]
     
     counts = count_de_novos(DENOVO_PATH, VALIDATIONS)
     neurodev = load_neurodevelopmental(neurodev_path)
     
-    neurodev["count"] = 0
+    neurodev["observed"] = 0
     for hgnc in counts:
         if hgnc in neurodev["hgnc"].values:
-            neurodev["count"][neurodev["hgnc"] == hgnc] = counts[hgnc]
+            neurodev["observed"][neurodev["hgnc"] == hgnc] = counts[hgnc]
     
     # plot LoF counts by mutation rates, shade by recognizability
-    neurodev = neurodev.merge(rates, how="left", left_on=["hgnc"], right_on=["gene"])
+    neurodev = neurodev.merge(expected, how="left", on=["hgnc"])
     
-    fig = seaborn.lmplot(x="rate", y="count", hue="Recognisable", data=neurodev,
-        size=6, hue_order=[1, 2, 3, 4, 5], y_jitter=0.1)
-    fig.savefig("temp.pdf", format="pdf")
+    # calculate the observed/expected ratio for individual genes
+    neurodev["ratio"] = neurodev["observed"]/neurodev["expected"]
+    
+    neurodev["ratio"][neurodev["ratio"].isnull()] = 0
+    
+    # join the group "1" with group "2", since both groups have fewer genes than
+    # the other groups.
+    recode = {1: "1+2", 2: "1+2", 3:3, 4:4, 5:5}
+    neurodev["Recognisable"] = neurodev["Recognisable"].map(recode)
+    
+    plot_recognisability(neurodev)
+    
+    missing = estimate_missing_variants(neurodev)
+    print(missing)
 
 if __name__ == '__main__':
     main()
