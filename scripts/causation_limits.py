860fb4bf8faa3a930fab430ef5f9bc1d7efd9605
Jeremy McRae <jm33@sanger.ac.uk>
2016-06-16 16:42:59 +0100

    standardise quotation marks

--- scripts/causation_limits.py
+++ scripts/causation_limits.py
@@ -1,4 +1,4 @@
-"""
+'''
 Copyright (c) 2015 Genome Research Ltd.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
@@ -17,7 +17,7 @@ FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
 IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
+'''
 
 from __future__ import absolute_import, print_function, division
 
@@ -43,41 +43,41 @@ from ddd_4k.causation.prevalence_estimates import prevalence_from_baseline_lof
 
 from mupit.mutation_rates import get_default_rates, get_expected_mutations
 
-UK_AGES = "/nfs/users/nfs_j/jm33/apps/ddd_4k/data/Father_Mother_AgesAtBirth.xlsx"
+UK_AGES = '/nfs/users/nfs_j/jm33/apps/ddd_4k/data/Father_Mother_AgesAtBirth.xlsx'
 
 def get_options():
-    """ parse the command line arguments
-    """
+    ''' parse the command line arguments
+    '''
     
-    parser = argparse.ArgumentParser(description="script to probe the limits " \
-        "of de novo causation in children with developmental disorders.")
-    parser.add_argument("--de-novos", default=DENOVO_PATH,
-        help="Path to table of candidate de novo mutations.")
-    parser.add_argument("--validations", default=VALIDATIONS,
-        help="Path to table of validation data.")
-    parser.add_argument("--trios", default=TRIOS,
-        help="Path to table of complete trios in the cohort.")
-    parser.add_argument("--sanger-ids", default=SANGER_IDS,
-        help="Path to table mapping between different ID formats.")
-    parser.add_argument("--phenotypes", default=PHENOTYPES,
-        help="Path to table of phenotypic data for probands.")
-    parser.add_argument("--known-genes", default=KNOWN_GENES,
-        help="Path to table of known developmental disorder genes.")
-    parser.add_argument("--previous-validations", default=PREVIOUS_VALIDATIONS,
-        help="Path to table of probands with known diagnostic variants.")
-    parser.add_argument("--constraints", default=CONSTRAINTS_URL,
-        help="Path or URL to table of constraint scores (pLI-based) for all" \
-            "genes in the genome.")
-    parser.add_argument("--uk-ages", default=UK_AGES,
-        help="Path to table of ages of parents at child's birth for mothers " \
-            "and fathers in the UK.")
+    parser = argparse.ArgumentParser(description='script to probe the limits '
+        'of de novo causation in children with developmental disorders.')
+    parser.add_argument('--de-novos', default=DENOVO_PATH,
+        help='Path to table of candidate de novo mutations.')
+    parser.add_argument('--validations', default=VALIDATIONS,
+        help='Path to table of validation data.')
+    parser.add_argument('--trios', default=TRIOS,
+        help='Path to table of complete trios in the cohort.')
+    parser.add_argument('--sanger-ids', default=SANGER_IDS,
+        help='Path to table mapping between different ID formats.')
+    parser.add_argument('--phenotypes', default=PHENOTYPES,
+        help='Path to table of phenotypic data for probands.')
+    parser.add_argument('--known-genes', default=KNOWN_GENES,
+        help='Path to table of known developmental disorder genes.')
+    parser.add_argument('--previous-validations', default=PREVIOUS_VALIDATIONS,
+        help='Path to table of probands with known diagnostic variants.')
+    parser.add_argument('--constraints', default=CONSTRAINTS_URL,
+        help='Path or URL to table of constraint scores (pLI-based) for all'
+            'genes in the genome.')
+    parser.add_argument('--uk-ages', default=UK_AGES,
+        help='Path to table of ages of parents at child\'s birth for mothers '
+            'and fathers in the UK.')
     
     args = parser.parse_args()
     
     return args
 
 def count_known_excess(filtered, known):
-    """ count the filtered de novos in DD-associated dominant genes.
+    ''' count the filtered de novos in DD-associated dominant genes.
     
     Protein altering de novos in DD-associated dominant genes are likely
     pathogenicand so must be part of the excess of de novo burden within the
@@ -89,10 +89,10 @@ def count_known_excess(filtered, known):
     
     Returns:
         dictionary of counts for truncating and 'missense' de novos.
-    """
+    '''
     
     dominant = known['gencode_gene_name'][known['mode'].isin(['Monoallelic', 'X-linked dominant'])]
-    variants = filtered[filtered["symbol"].isin(dominant)].copy()
+    variants = filtered[filtered['symbol'].isin(dominant)].copy()
     
     counts = variants['category'].value_counts()
     counts['missense'] = counts['functional']
@@ -112,14 +112,14 @@ def print_known_in_excess(in_dominant, excess):
         100 * (lof_in_dominant + mis_in_dominant)/(lof_excess + mis_excess)))
 
 def proportion_in_dominant_hi_genes(filtered, known):
-    """ count the consequence types in dominant haploinsufficient DD genes
+    ''' count the consequence types in dominant haploinsufficient DD genes
     
     Within the set of dominant haploinsufficient DD-associated genes, examine
     the breakdown of loss-of-function and missense consequences.
     
     Returns:
         dictionary of counts for truncating and 'missense' de novos
-    """
+    '''
     
     dominant = known[known['mode'].isin(['Monoallelic', 'X-linked dominant'])]
     dominant_haploinsufficient = dominant['gencode_gene_name'][dominant['mech'] == 'Loss of function']
@@ -173,14 +173,16 @@ def main():
     
     de_novos = open_de_novos(args.de_novos, args.validations, exclude_synonymous=False)
     known = open_known_genes(args.known_genes)
-    de_novos["known"] = de_novos["symbol"].isin(known["gencode_gene_name"])
-    de_novos["start_pos"] = de_novos["pos"]
+    de_novos['known'] = de_novos['symbol'].isin(known['gencode_gene_name'])
+    de_novos['start_pos'] = de_novos['pos']
     constraints = pandas.read_table(args.constraints)
+    # constraints['pLI'] = -constraints['ALL_0.1%'] # for RVIS-scores
+    # constraints['gene'] = constraints['GENE'] # for RVIS-scores
     uk_ages = open_uk_parent_ages(args.uk_ages)
     
     phenotypes = open_phenotypes(args.phenotypes, args.sanger_ids)
     trios = pandas.read_table(args.trios)
-    phenotypes = phenotypes[phenotypes["patient_id"].isin(trios["decipher_id"])]
+    phenotypes = phenotypes[phenotypes['patient_id'].isin(trios['decipher_id'])]
     
     male = 2407
     female = 1886
@@ -188,7 +190,7 @@ def main():
     expected = get_expected_mutations(rates, male, female)
     
     pp_dnm_threshold = get_pp_dnm_threshold(de_novos, expected)
-    filtered = de_novos[(~de_novos["pp_dnm"].isnull() & (de_novos["pp_dnm"] > pp_dnm_threshold)) ]
+    filtered = de_novos[(~de_novos['pp_dnm'].isnull() & (de_novos['pp_dnm'] > pp_dnm_threshold)) ]
     
     complete = open_de_novos(args.de_novos, exclude_synonymous=False)
     validations = open_previous_validations(args.previous_validations, complete)
@@ -198,7 +200,7 @@ def main():
     functional_excess = excess['truncating']['excess'] + excess['missense']['excess']
     snv_yield = functional_excess / (male + female)
     
-    plot_consequence_excess(excess, "results/excess_by_consequence.pdf")
+    plot_consequence_excess(excess, 'results/excess_by_consequence.pdf')
     plot_excess_by_pp_dnm_threshold(de_novos, expected, validations, increments=100)
     
     dominant_hi = proportion_in_dominant_hi_genes(filtered, known)
@@ -238,8 +240,8 @@ def main():
     
     plot_prevalence_by_age(prevalance_from_rates, phenotypes, uk_ages,
         reference_mutations, dad_rate=1.53, mom_rate=0.86)
-    print("prevalence: {}".format(prevalance_from_rates))
-    print("prevalence 95% CI: {}".format(ci))
+    print('prevalence: {}'.format(prevalance_from_rates))
+    print('prevalence 95% CI: {}'.format(ci))
     
     print('SNV yield: {}'.format(snv_yield))
     
